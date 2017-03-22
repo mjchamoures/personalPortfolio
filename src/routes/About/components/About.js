c6c79c7a27fdde5d92bd5101d901db3c21575553
mchamour <mchamour@cisco.com>
2017-03-21 22:48:21 -0700

    remove unneeded bootstrap imports from About component

--- src/routes/About/components/About.js
+++ src/routes/About/components/About.js
@@ -2,15 +2,8 @@ import React from 'react'
 import { ContactMe } from './ContactMe';
 import { BasicInfo } from './BasicInfo';
 import { Resume } from './Resume';
-import { Form } from 'react-bootstrap';
-import { FormControl } from 'react-bootstrap';
-import { FormGroup } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import { ListGroup } from 'react-bootstrap';
-import { ListGroupItem } from 'react-bootstrap';
-import { ControlLabel } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
-import { Panel } from 'react-bootstrap';
+
 
 export const About = (props) => (
 
