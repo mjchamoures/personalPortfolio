440dac76ded9ee5197193bed84daf8697c72d877
mchamour <mchamour@cisco.com>
2017-04-10 20:01:42 -0700

    Configure for heroku deployment

--- config/project.config.js
+++ config/project.config.js
@@ -24,8 +24,8 @@ const config = {
   // ----------------------------------
   // Server Configuration
   // ----------------------------------
-  server_host : ip.address(), // use string 'localhost' to prevent exposure on local network
-  server_port : process.env.PORT || 3000,
+  server_host : process.env.HOST || ip.address(), // use string 'localhost' to prevent exposure on local network
+  server_port : process.env.PORT || 2000,
 
   // ----------------------------------
   // Compiler Configuration
