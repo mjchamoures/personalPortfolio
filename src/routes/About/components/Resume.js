599d88111d19ce02814b2ee2d611f6b1b12dc391
mchamour <mchamour@cisco.com>
2017-04-10 20:27:29 -0700

    update resume

--- src/routes/About/components/Resume.js
+++ src/routes/About/components/Resume.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Panel } from 'react-bootstrap';
 import ResumeImg from '../assets/Michael_Chamoures_Resume.png';
+import ResumeImg2 from '../assets/Michael_Chamoures_Resume_2.png';
 import './About.scss';
 
 
@@ -12,6 +13,11 @@ export const Resume = (props) => (
       alt=''
       className='resumeImg'
       src={ResumeImg}
+    />
+    <img
+      alt=''
+      className='resumeImg'
+      src={ResumeImg2}
     />
 
   </Panel>
