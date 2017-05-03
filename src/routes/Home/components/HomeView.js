eae8b7df17789e2b25b3ab84d6cddeedb616eb9c
mchamour <mchamour@cisco.com>
2017-05-03 09:16:28 -0700

    Add home intro text

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -2,13 +2,20 @@ import React from 'react'
 import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
 
+import { Col, Row } from 'react-bootstrap';
+
 export const HomeView = () => (
   <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className='duck'
-      src={DuckImage} />
+    <Col xs={12} className={"home-view-title"}>
+        <h3>Welcome!</h3>
+    </Col>
+
+    <Col xs={12} className={"home-view-intro"}>
+        <p>I'm a developer based in San Francisco, CA</p>
+        <p>This is a simple site I put together using React.js</p>
+        <p>It's still a work in progress, but feel free to check out some other projects I've worked on using the nav links above</p>
+        <p>Thanks for stopping by!</p>   
+    </Col>
   </div>
 )
 
